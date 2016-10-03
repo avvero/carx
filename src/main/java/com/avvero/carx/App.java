55a9f25035b71d991b22ee55f0171c4750442471
avvero <avvero@mail.ru>
2016-10-03 21:12:37 +0700

    first

---
+++ src/main/java/com/avvero/carx/App.java
@@ -0,0 +1,20 @@
+package com.avvero.carx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author Avvero
+ */
+@ComponentScan
+@EnableAutoConfiguration
+@EnableJpaRepositories
+public class App {
+
+    public static void main(String args[]) throws Throwable {
+        SpringApplication.run(App.class, args);
+    }
+
+}
